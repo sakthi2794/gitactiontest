e9a7c92b578d090049bb5f011a55d8988c476616
sakthi2794 <sakthi.rajasekar@anywhere.co>
2020-07-08 17:20:09 +0530

    Initial commit

---
+++ src/main/java/setmoresignup/Signup.java
@@ -0,0 +1,47 @@
+package setmoresignup;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Signup {
+	
+	public static void main(String args[]) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+	
+		
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+		driver.get("https://my.setmore.com/logout.do");
+		System.out.println("Landed to Setmore Login page");
+		driver.findElement(By.id("username")).sendKeys("rsakthi1994@gmail.com");
+		driver.findElement(By.id("password")).sendKeys("sakthi27");
+		driver.findElement(By.id("login-now")).click();
+		System.out.println("Logging in ");
+		Thread.sleep(10000);
+		driver.findElement(By.xpath("//li[@id='account_menu']/a[@class='right_nav_holder-loginid']")).click();
+		driver.findElement(By.xpath("//ul[@class='nav-topsub-navigation active-dropdown']//li[@id='sign_out']/a")).click();
+		System.out.println("Clicked on Logout");
+		Thread.sleep(5000);
+		String currentUrl = driver.getCurrentUrl();
+		if(currentUrl.equalsIgnoreCase("https://my.setmore.com/logout.do"))
+		{
+			System.out.println("Logged out successfully");
+		}
+		else
+		{
+			System.out.println("Logout fail");
+		}
+		
+		
+		
+	}
+
+}
